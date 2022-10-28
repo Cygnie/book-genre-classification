a86891f923100e40a3c9b55d9efbd184d0be5805
Furkan CEYRAN <75915883+Cygnie@users.noreply.github.com>
2022-10-28 20:48:07 +0300

    app and requirements files updated for streamlit

--- app.py
+++ app.py
@@ -1,41 +1,40 @@
-from flask import Flask, request,app,jsonify,url_for,render_template
 import numpy as np
+import streamlit as st
 from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
-
-
-
-app = Flask(__name__)
+import warnings
+warnings.filterwarnings("ignore")
 
 # Load the Tokenizer from hugging face hub
 TOKENIZER = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
 # Load the model
-model = TFDistilBertForSequenceClassification.from_pretrained("book_class_saved_model_2022-10-23 19_46_55.232799.bin")
+model = TFDistilBertForSequenceClassification.from_pretrained("model.bin")
 
 label = open("label.txt","r").read()
 id2label = {key:value for key,value in enumerate(label.split("\n"))}
 
+def predict(summary,id2label):
 
-@app.route("/")
-def homePage():
-    return render_template("home.html")
-
-@app.route('/predict_api',methods=["POST"])
-
-def predict_api():
-    data = request.json["data"]
-    print(data)
-
-    tokenized_data = TOKENIZER(list(data.values()), truncation = True, max_length = 128, padding= True, return_tensors = "tf")
+    tokenized_data = TOKENIZER([summary], truncation = True, max_length = 128, padding= True, return_tensors = "tf")
 
     prediction = model(tokenized_data).logits
 
     classifications = np.argmax(prediction, axis=1)
-    print(classifications)
 
     output = id2label[classifications[0]]
-    return jsonify(output)
+    return output
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def main():
+    st.set_page_config(page_title="Book Genre Classification")
+    st.markdown("<h1 style='text-align:center;'>Book Genre Classification</h1>",unsafe_allow_html=True)
+    st.write("Please fill in the text field with your book summary")
+    summary = st.text_area("summary")
+
+    result=""
+    if st.button("Predict"):
+        result=predict(summary,id2label)
+    st.success('The output is {}'.format(result))
+
+if __name__ == '__main__':
+    main()
     
